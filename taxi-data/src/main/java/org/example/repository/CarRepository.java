3dcd837af4ff7eb4d72fee250d4187b4ac30f696
DESKTOP-0J0G6T3\Roman Mikhailovskikh <romanunylov666@gmail.com>
2023-05-05 11:34:40 +0500

    tao bao

--- taxi-data/src/main/java/org/example/repository/CarRepository.java
+++ taxi-data/src/main/java/org/example/repository/CarRepository.java
@@ -1,35 +1,47 @@
 package org.example.repository;
 
-import org.example.entity.Car;
-
+import org.example.dao.CarDao;
+import org.example.dto.CarDto;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
-public class CarRepository implements ICarRepository {
-    private EntityManager entityManager;
-    public CarRepository(EntityManager entityManager) {
-        this.entityManager = entityManager;
+public class CarRepository {
+    private CarDao dao;
+
+    public CarRepository(CarDao dao)
+    {
+        this.dao = dao;
     }
 
-    public List<Car> getAll() {
-        var cars = this.entityManager.createQuery("""
-                select c
-                from cars c""")
-                .getResultList();
-
-        return cars;
+    public CarDto create(CarDto car)
+    {
+        return car;
     }
 
-//    @Transactional
-//    public Car addCar(CarDto carDto){
-//        var car = CarMapper.INSTANCE.toEntity(carDto);
-//        car = entityManager.merge(car);
-//
-//        var newCarDto = CarMapper.INSTANCE.toDto(car);
-//
-//        return newCarDto;
-//    }
+    public CarDto update(CarDto car)
+    {
+        return car;
+    }
+
+    public CarDto getById(Long id)
+    {
+        CarDto dto = new CarDto();
+        return dto;
+    }
+
+    public List<CarDto> getAll(Long id)
+    {
+        List<CarDto> dtoList = new ArrayList<CarDto>();
+        return dtoList;
+    }
+
+    public void delete(Long id)
+    {
+        
+    }
+
+
 }
